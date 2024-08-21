ec6b3c2d8db90a4100ea493d4a2ef7722d7c064c
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-08-21 18:01:50 +0000

    feat(player): disable nametag

---
+++ src/client/java/world/anhgelus/molehunt/client/mixin/NoNametags.java
@@ -0,0 +1,21 @@
+package world.anhgelus.molehunt.client.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public abstract class NoNametags<T extends Entity> {
+    @Inject(at = @At("HEAD"), method = "render", cancellable = true)
+    private void renderLabelOrNot(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        if (!(entity instanceof PlayerEntity)) return;
+
+        ci.cancel();
+    }
+}
