b504d77be3eafe23dbbab6c69ca9f23aca6111fe
Léo Kosman <leo.kosman@proton.me>
2024-08-22 23:35:37 +0200

    style: revert subtitle back to *The* moles

--- src/main/java/world/anhgelus/molehunt/Game.java
+++ src/main/java/world/anhgelus/molehunt/Game.java
@@ -7,7 +7,6 @@ import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextContent;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.GameRules;
 import world.anhgelus.molehunt.utils.TimeUtils;
@@ -136,7 +135,7 @@ public class Game {
                 } else {
                     winner = new TitleS2CPacket(Text.of("§aNot the Mole!"));
                 }
-                pm.sendToAll(new SubtitleS2CPacket(Text.of("§6Moles were " + getMolesAsString())));
+                pm.sendToAll(new SubtitleS2CPacket(Text.of("§6The Moles were " + getMolesAsString())));
                 pm.sendToAll(winner);
                 pm.sendToAll(timing);
             }
