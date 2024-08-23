7fd5caf38a9b3ef71d94d9085ee92b47c3c449e3
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-08-23 13:42:56 +0000

    feat(config): disable mixins according to config's packet

--- src/client/java/world/anhgelus/molehunt/client/mixin/NoNametags.java
+++ src/client/java/world/anhgelus/molehunt/client/mixin/NoNametags.java
@@ -9,12 +9,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import world.anhgelus.molehunt.client.MolehuntClient;
 
 @Mixin(EntityRenderer.class)
 public class NoNametags<T extends Entity> {
     @Inject(at = @At("HEAD"), method = "render", cancellable = true)
     private void renderLabelOrNot(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-        if (!(entity instanceof PlayerEntity)) return;
+        if (!(entity instanceof PlayerEntity) || MolehuntClient.showNameTags()) return;
         ci.cancel();
     }
 }
--- src/client/java/world/anhgelus/molehunt/client/mixin/NoPlayerListHud.java
+++ src/client/java/world/anhgelus/molehunt/client/mixin/NoPlayerListHud.java
@@ -5,11 +5,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import world.anhgelus.molehunt.client.MolehuntClient;
 
 @Mixin(PlayerListHud.class)
 public class NoPlayerListHud {
     @Inject(at = @At("HEAD"), method = "render", cancellable = true)
     public void render(CallbackInfo ci) {
+        if (MolehuntClient.showTab()) return;
         ci.cancel();
     }
 }
