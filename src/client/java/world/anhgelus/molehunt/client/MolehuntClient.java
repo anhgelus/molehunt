fdb9a0174c22499f545a0426d6d72148cf76411c
Léo Kosman <leo.kosman@proton.me>
2024-08-31 22:44:07 +0200

    feat: no no skin overlay

--- src/client/java/world/anhgelus/molehunt/client/MolehuntClient.java
+++ src/client/java/world/anhgelus/molehunt/client/MolehuntClient.java
@@ -1,7 +1,9 @@
 package world.anhgelus.molehunt.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.entity.player.PlayerModelPart;
 import world.anhgelus.molehunt.config.ConfigPayload;
 import world.anhgelus.molehunt.game.GamePayload;
 
@@ -15,18 +17,26 @@ public class MolehuntClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ClientPlayNetworking.registerGlobalReceiver(ConfigPayload.ID, (payload, context) -> {
-            context.client().execute(() -> {
-                SHOW_SKINS = payload.showSkins();
-                SHOW_NAMETAGS = payload.showNametags();
-                SHOW_TAB = payload.showTab();
-            });
+        ClientPlayNetworking.registerGlobalReceiver(ConfigPayload.ID, (payload, context) -> context.client().execute(() -> {
+            SHOW_SKINS = payload.showSkins();
+            SHOW_NAMETAGS = payload.showNametags();
+            SHOW_TAB = payload.showTab();
+        }));
+        ClientPlayNetworking.registerGlobalReceiver(GamePayload.ID, (payload, context) -> context.client().execute(() -> GAME_STARTED = payload.gameLaunched()));
+
+        // Needed because else `client.options` is null
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            var options = client.options;
+
+            options.togglePlayerModelPart(PlayerModelPart.CAPE, true);
+            options.togglePlayerModelPart(PlayerModelPart.HAT, true);
+            options.togglePlayerModelPart(PlayerModelPart.JACKET, true);
+            options.togglePlayerModelPart(PlayerModelPart.LEFT_SLEEVE, true);
+            options.togglePlayerModelPart(PlayerModelPart.RIGHT_SLEEVE, true);
+            options.togglePlayerModelPart(PlayerModelPart.LEFT_PANTS_LEG, true);
+            options.togglePlayerModelPart(PlayerModelPart.RIGHT_PANTS_LEG, true);
         });
-        ClientPlayNetworking.registerGlobalReceiver(GamePayload.ID, (payload, context) -> {
-            context.client().execute(() -> {
-                GAME_STARTED = payload.gameLaunched();
-            });
-        });
+
     }
 
     public static boolean showSkins() {
